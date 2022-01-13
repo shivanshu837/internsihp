deba3f53948ec8cafa106ef933d39c0fc9549d85
shivanshu837 <54849633+shivanshu837@users.noreply.github.com>
2022-01-13 12:38:15 +0530

    Add files via upload

--- Ip_netmasking.py
+++ Ip_netmasking.py
@@ -1,12 +1,19 @@
-import socket
+import subprocess
 import json
 
-hostname = socket.gethostname()
+data = subprocess.check_output(['ipconfig', '/all']).decode('utf-8').split('\n')
 
-ip_address = socket.gethostbyname(hostname)
+ip_config = {}
 
-subnetmask = ".".join(ip_address.split(".")[:3])
 
-ip = {"hostname" : hostname, "ip_address" : ip_address, "sub_net_mask" : subnetmask}
+for item in data:
+    if item.find('Host Name') != -1:
+        ip_config['Host Name'] = item.split(':')[1]
+        
+    if item.find('IPv4 Address') != -1:
+        ip_config['IPv4 Address'] = item.split(':')[1]
+        
+    if item.find('Subnet Mask') != -1:
+        ip_config['Subnet Mask'] = item.split(':')[1]
 
-print(json.dumps(ip))
+print(json.dumps(ip_config))
